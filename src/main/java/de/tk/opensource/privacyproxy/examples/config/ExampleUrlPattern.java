793c1efc22c561ec7e4aa33b327620c41fdf767a
Kassim Hölting <kassimhoelting@users.noreply.github.com>
2019-08-23 16:28:01 +0200

    make sonarqube happye

---
+++ src/main/java/de/tk/opensource/privacyproxy/examples/config/ExampleUrlPattern.java
@@ -0,0 +1,16 @@
+/*--- (C) 1999-2019 Techniker Krankenkasse ---*/
+
+package de.tk.opensource.privacyproxy.examples.config;
+
+import de.tk.opensource.privacyproxy.config.UrlPattern;
+
+public class ExampleUrlPattern extends UrlPattern {
+	public class Provider {
+		public static final String EXAMPLE = "/example";
+
+		private Provider() {
+		}
+	}
+}
+
+/*--- Formatiert nach TK Code Konventionen vom 05.03.2002 ---*/
